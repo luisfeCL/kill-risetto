8f6db203e2ba83578212e1f394f8138e2f52b3a6
luisfeCL <luisfepca@gmail.com>
2025-09-30 10:59:02 +0200

    dlete comments

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,6 @@ export default tseslint.config(
         wc: wcPlugin,
         },
         rules: {
-        // Reglas generales
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error', { 
             argsIgnorePattern: '^_',
@@ -24,20 +23,14 @@ export default tseslint.config(
         'no-var': 'error',
         'eqeqeq': ['error', 'always'],
         'curly': ['error', 'all'],
-        
-        // Reglas para LitElement
         'lit/no-invalid-html': 'error',
         'lit/no-useless-template-literals': 'error',
         'lit/attribute-value-entities': 'error',
         'lit/binding-positions': 'error',
         'lit/no-property-change-update': 'error',
-        
-        // Reglas para Web Components
         'wc/no-invalid-element-name': 'error',
         'wc/no-self-class': 'error',
         'wc/guard-super-call': 'error',
-        
-        // Estilo
         'indent': ['error', 2],
         'quotes': ['error', 'single'],
         'semi': ['error', 'always'],
